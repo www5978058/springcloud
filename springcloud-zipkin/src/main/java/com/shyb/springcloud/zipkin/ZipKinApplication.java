3d8468c12e658f56894555ee1bcd563c63264020
17396387378 <515030185@qq.com>
2019-06-27 11:53:31 +0800

    zipkin 配置

--- springcloud-zipkin/src/main/java/com/shyb/springcloud/zipkin/ZipKinApplication.java
+++ springcloud-zipkin/src/main/java/com/shyb/springcloud/zipkin/ZipKinApplication.java
@@ -12,7 +12,7 @@ import zipkin.server.internal.EnableZipkinServer;
 @EnableZipkinServer
 @EnableEurekaClient
 @SpringBootApplication
-public class ZipKinApplication {
+public class    ZipKinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipKinApplication.class,args);
     }
